b9c4bed4e452f8cf238ebf7ba95867155855846a
coderoofsit <141501346+coderoofsit@users.noreply.github.com>
2025-09-25 19:23:41 +0530

    update the url of backend

--- src/app/config/config.js
+++ src/app/config/config.js
@@ -1,3 +1,5 @@
 // export const BASE_URL = "http://localhost:8000";
 
-export const BASE_URL = "https://exampur.onrender.com"
+// export const BASE_URL = "https://exampur.onrender.com"
+
+export const BASE_URL = "https://exampre-backend.onrender.com"
